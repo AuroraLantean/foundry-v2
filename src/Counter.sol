4a28119587e89f566ef81d28112077a30981e39b
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-07-31 14:52:33 +0800

    setup dependencies and remappings.txt

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
+import "forge-std/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+
+contract ERC20Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 9000000000 * 10 ** 18);
+    }
+}
 contract Counter {
     uint256 public number;
 
